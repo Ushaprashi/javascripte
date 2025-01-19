45df89313c1e1d992c0d0d1f4f5160c047ac10d0
Ushaprashi <raniusharani502@gmail.com>
2025-01-19 13:22:33 +0530

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,29 @@
+var sid=parseInt(prompt("enter the id"));
+        var sname=prompt("enter the name")
+        var m1=parseInt(prompt("enter the m1"));
+        var m2=parseInt(prompt("enter the m2"));
+        var m3=parseInt(prompt("enter the m3"));
+        var m4=parseInt(prompt("enter the m4"));
+        var m5=parseInt(prompt("enter the m5"));
+        var total=m1+m2+m3+m4+m5;
+        var percentage=(total*100/500.0);
+        document.write("Student id:"+sid+"<br>");
+        document.write("Student Name:"+sname+"<br>");
+        document.write("Total:"+total+"<br>");
+        document.write("Percentage:"+percentage+"<br>");
+        let grade;
+        if (percentage >90) {
+         alert (" Grade A")
+        } else if (percentage >80) {
+            alert (" Grade B")
+        } else if (percentage >70) {
+            alert (" Grade C")
+        } else if (percentage >60) {
+            alert (" Grade D")
+        } else if (percentage >50) {
+            alert (" Grade E") 
+
+        } else {
+            alert ( Grade = 'fail')
+        }
+      
